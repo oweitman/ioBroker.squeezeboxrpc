9550bef23ddbd3878f9385789474b806d4bf9c76
oweitman <oweitman@gmx.de>
2026-09-08 00:52:31 +0200

    remove comment

--- widgets/squeezeboxrpc/build/squeezeboxrpc-dist.js
+++ widgets/squeezeboxrpc/build/squeezeboxrpc-dist.js
@@ -1361,7 +1361,7 @@
   }
 
   // ../package.json
-  var version = "2.0.1";
+  var version = "2.0.2";
 
   // squeezeboxrpc/js/textImage.js
   String.prototype.regexIndexOf = function(regex, startpos) {
